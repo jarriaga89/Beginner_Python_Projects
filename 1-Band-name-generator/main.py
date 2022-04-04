de30ebeceab6b6f1dcccac3a342df0fe764f204b
jarriaga89 <102043390+jarriaga89@users.noreply.github.com>
2022-04-04 10:12:33 -0400

    Delete 1-Band-name-generator directory

--- 1-Band-name-generator/main.py
+++
@@ -1,7 +0,0 @@
-print("Hello there. Welcome to Josh's SIMPLE Band Name Generator. This will generate a band name for you.")
-
-city = input("What's the name of the city you were born? \n")
-
-pet = input("Great. What's the name of your first pet? \n")
-
-print("Awesome! Your Band name is " +city +" "+ pet)
